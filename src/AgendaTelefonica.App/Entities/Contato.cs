a4d124e96122485dbc6df75a9c95729822fc7d6a
Aislan Michel Moreira Freitas <aislan.michel92@gmail.com>
2022-06-06 21:37:10 -0300

    adicionando classe de entidade kkkk

---
+++ src/AgendaTelefonica.App/Entities/Contato.cs
@@ -0,0 +1,23 @@
+﻿namespace AgendaTelefonica.App.Entities;
+
+public class Contato 
+{
+    public Contato(string nome, string telefone, string email)
+    {
+        Id = Guid.NewGuid();
+        Nome = nome;
+        Telefone = telefone;
+        Email = email;
+
+    }
+
+    public Guid Id { get; private set; }
+    public string Nome { get; private set; }
+    public string Telefone { get; private set; }
+    public string Email { get; private set; }
+    
+    public override string ToString() 
+    {
+        return $"Nome: {Nome} | Id: {Id} | Telefone: {Telefone} | E-mail: {Email}";
+    }
+}
